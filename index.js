c3ac9aeb5369aad007c20768c28ee844a2bab709
mtrimner <matt.trimner@gmail.com>
2020-11-17 11:43:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function writeCards(names, events) {
+    let thankYouMessage = []
+    for (let i = 0; i < names.length; i++) {
+       thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return thankYouMessage
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > 0 ) {
+        console.log(startingNumber);
+        startingNumber--;
+    }
+    console.log(startingNumber)
+}
